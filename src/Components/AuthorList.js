6e722fd979e4c0230f14b3831dcac99c17746fca
reduviidae <wiljago@gmail.com>
2019-05-06 16:06:44 -0400

    incorporating lazy loading

---
+++ src/Components/AuthorList.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { PLAIN_HEADERS } from "../constants";
+
+class AuthorList extends Component {
+  componentDidMount() {
+    this.fetchList();
+  }
+
+  fetchList = () => {
+    fetch(`${process.env.REACT_APP_EPRH_AUTH_URL}`, {
+      method: "GET",
+      headers: PLAIN_HEADERS
+    })
+      .then(r => r.json())
+      .then(this.props.authorsToState);
+  };
+
+  render() {
+    console.log(this.props.data.data && this.props.data.data.authors);
+    const authorList =
+      this.props.data.data &&
+      this.props.data.data.authors.map(author => {
+        return (
+          <div class="author_list_div">
+            <h3>{author.display}</h3>
+            <a
+              href={`https://www.penguinrandomhouse.com${
+                author.seoFriendlyUrl
+              }`}
+            >
+              PRH Website Link
+            </a>
+          </div>
+        );
+      });
+    return <div>{authorList}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    data: state.data
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    authorsToState: data => dispatch({ type: "GET_AUTHORS", payload: data })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AuthorList);
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,49 +1,21 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 
-import { PLAIN_HEADERS } from "../constants";
+const AuthorList = React.lazy(() => {
+  import(/* webpackChunkName: 'AuthorList' */ "./AuthorList" )
+})
 
 class Main extends Component {
-  componentDidMount () {
-    this.fetchList();
-  }
-
-  fetchList = () => {
-    fetch(`${process.env.REACT_APP_EPRH_AUTH_URL}`, {
-      method: 'GET',
-      headers: PLAIN_HEADERS
-    })
-    .then(r => r.json())
-    .then(this.props.authorsToState)
-  }
 
   render() {
-    console.log(this.props.data);
-    const authors = this.props.data.data && this.props.data.data.authors.map(author => {
-      return (<div class="author_list_div">
-        <h3>{author.display}</h3>
-        <a href={`https://www.penguinrandomhouse.com${author.seoFriendlyUrl}`}>Penguin Website Link</a>
-      </div>)
-    })
     return (
-      <div>
+      <React.Suspense fallback={<div>Loading...</div>}>
         <h1>List of Selected PRH Authors</h1>
-        {authors}
-      </div>
+        <AuthorList />
+      </React.Suspense>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-      data: state.data
-  };
-}
 
-const mapDispatchToProps = dispatch => {
-  return {
-    authorsToState: data => dispatch({type: "GET_AUTHORS", payload: data})
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default Main;
