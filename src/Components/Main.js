213a865215aee0174ee29564e1c92e53ed737900
reduviidae <wiljago@gmail.com>
2019-05-06 13:58:11 -0400

    fetching authors from EPRH API

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { PLAIN_HEADERS } from "../constants";
+
 class Main extends Component {
+  componentDidMount () {
+    this.fetchList();
+  }
+
+  fetchList = () => {
+    fetch(`${process.env.REACT_APP_EPRH_AUTH_URL}`, {
+      method: 'GET',
+      headers: PLAIN_HEADERS
+    })
+    .then(r => r.json())
+    .then(console.log)
+  }
+
   render() {
     return (
       <div>
         <h1>{this.props.data}</h1>
-        <h1>{process.env.REACT_APP_TEST}</h1>
       </div>
     );
   }
